c1a7143ff30e9f9a89028c6d6ee11db80f97e550
Sam Garson <samtgarson@gmail.com>
2023-05-15 15:11:11 +0100

    Upgrade deps and remove deprecated rule

--- typescript.js
+++ typescript.js
@@ -1,33 +1,35 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript'
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/member-delimiter-style': [
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/member-delimiter-style": [
       2,
       {
         multiline: {
-          delimiter: 'none'
+          delimiter: "none",
         },
         singleline: {
-          delimiter: 'semi'
-        }
-      }
+          delimiter: "semi",
+        },
+      },
     ],
-    '@typescript-eslint/no-use-before-define': 0,
-    '@typescript-eslint/no-empty-function': 0,
-    '@typescript-eslint/ban-ts-comment': 1,
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': ['warn', {
-      ignoreArgsIfArgsAfterAreUsed: true
-    }]
-
-  }
-}
+    "@typescript-eslint/no-use-before-define": 0,
+    "@typescript-eslint/no-empty-function": 0,
+    "@typescript-eslint/ban-ts-comment": 1,
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "after-used",
+        argsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+  },
+};
